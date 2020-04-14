d9660025ddff3f66c82dc99be4408c2f6a593b32
Artyom Opanchuk <subzero666@list.ru>
2020-04-14 16:41:03 +0300

    Fix requirements

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'django>=1.11,<3.0',
+        'Django>=1.11,<3.0',
         'django-cms>=3.2.3,<4.0',
         'django-appconf>=1.0.1,<2.0',
-        'jsonfield>=2.1,<3.0',
+        'jsonfield>=2.1,<4.0',
     ],
     license="BSD",
     zip_safe=False,
